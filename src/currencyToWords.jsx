26917dc890c429215c505beba97f7ca17951c067
Meisam Malekzadeh <meisammalekzadeh@gmail.com>
2020-06-09 23:23:56 +0430

    seperate converter functions

--- src/currencyToWords.jsx
+++ src/currencyToWords.jsx
@@ -1,44 +1,45 @@
-import {  oneDigit,  twoDigits,  threeDigits,  GetNumericGroupTitle} from "./converter";
+import {  oneDigit,  twoDigits,  threeDigits,  GetNumericGroupTitle} from "./converter.jsx";
 
-const CurrencyToWords = (value) => {
-  const convertToWords = (currency) => {
-    if (currency !=0 && !currency) return "invalid input";
+const convertToWords = (currency) => {
+  if (currency !=0 && !currency) return "invalid input";
 
-    let numericGroup = 1; // thousands , millions
-    let result = "";
+  let numericGroup = 1; // thousands , millions
+  let result = "";
 
-    try {
-      while (currency != 0) {
-        let tempResult = "";
-        let remain = currency % 1000;
+  try {
+    while (currency != 0) {
+      let tempResult = "";
+      let remain = currency % 1000;
 
-        if (remain < 10) tempResult = oneDigit(remain);
-        else if (remain < 100) tempResult = twoDigits(remain);
-        else tempResult = threeDigits(remain);
+      if (remain < 10) tempResult = oneDigit(remain);
+      else if (remain < 100) tempResult = twoDigits(remain);
+      else tempResult = threeDigits(remain);
 
-        if (tempResult)
-          //for numbers like 1,000,001 that thousands part is empty
-          result = `${tempResult} ${GetNumericGroupTitle(numericGroup)} ${result}`.trim();
+      if (tempResult)
+        //for numbers like 1,000,001 that thousands part is empty
+        result = `${tempResult} ${GetNumericGroupTitle(numericGroup)} ${result}`.trim();
 
-        currency = Math.floor(currency / 1000);
-        numericGroup++;
-      }
-      return result;
-    } catch (e) {
-      throw new Error("Exception in parsing");
+      currency = Math.floor(currency / 1000);
+      numericGroup++;
     }
-  };
+    return result;
+  } catch (e) {
+    throw new Error("Exception in parsing");
+  }
+};
 
-  const completeDollarWords = (dollars) => {
-    if (!dollars) dollars = "zero";
-    return dollars + (dollars === "one" ? " dollar" : " dollars");
-  };
+const completeDollarWords = (dollars) => {
+  if (!dollars) dollars = "zero";
+  return dollars + (dollars === "one" ? " dollar" : " dollars");
+};
 
-  const completeCentsWords = (cents) => {
-    if (!cents) return "";
-    return ` and ${cents} ` + (cents === "one" ? "cent" : "cents");
-  };
+const completeCentsWords = (cents) => {
+  if (!cents) return "";
+  return ` and ${cents} ` + (cents === "one" ? "cent" : "cents");
+};
 
+export const CurrencyToWords = (value) => {
+  
   const currency = (value + "").replace(" ", "").split(".");
 
   const dollarInWords = convertToWords(Number.parseInt(currency[0]));
